d40212f928386607ccbb7650d2db6cac05ea20e5
sharuma23 <sharuma23@DESKTOP-3OQDP2D.localdomain>
2022-06-16 15:05:57 -0400

    fuck fuck fuck

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,25 +1,5 @@
-import { Link } from "@remix-run/react";
-import ProfileBubble from "~/components/profileBubble";
-import { useState, useEffect } from "react";
-
 export default function Index() {
-
-  return (
-    //sm:flex sm:items-center sm:justify-center
-    <main className="relative min-h-screen bg-blue-400 flex">
-      <ProfileBubble/>
-      <ProfileBubble/>
-      <ProfileBubble/>
-      <ProfileBubble/>
-      <ProfileBubble/>
-      <ProfileBubble/>
-      <ProfileBubble/>
-      <ProfileBubble/>
-      <ProfileBubble/>
-      <ProfileBubble/>
-      <ProfileBubble/>
-      <ProfileBubble/>
-
-    </main>
-  );
+  return(
+    <></>
+  )
 }
